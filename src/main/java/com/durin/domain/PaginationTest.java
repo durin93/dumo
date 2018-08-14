50a81ee8075ac2c86ab9c76bd0c8e1574df23eb1
durin93 <lsc401@naver.com>
2018-08-14 16:24:31 +0900

    update paginationTest

--- src/main/java/com/durin/domain/PaginationTest.java
+++ src/main/java/com/durin/domain/PaginationTest.java
@@ -16,7 +16,7 @@ public class PaginationTest {
 		pagination = Pagination.of(0, 1L);
 		assertThat(pagination.leftButton(), is(""));
 		pagination = Pagination.of(4, 1L);
-		assertThat(pagination.leftButton(), is("<li><a href=/memos/"+1L+"/"+3+"><</a></li>"));
+		assertThat(pagination.leftButton(), is("<li><a href=/api/memos/"+1L+"/"+3+"><</a></li>"));
 	}
 
 	@Test
@@ -24,17 +24,17 @@ public class PaginationTest {
 		pagination = Pagination.of(11, 1L);
 		assertThat(pagination.rightButton(10), is(""));
 		pagination = Pagination.of(0, 1L);
-		assertThat(pagination.rightButton(10), is("<li><a href=/memos/"+1L+"/"+(1)+">></a></li>"));
+		assertThat(pagination.rightButton(10), is("<li><a href=/api/memos/"+1L+"/"+(1)+">></a></li>"));
 		pagination = Pagination.of(5, 1L);
-		assertThat(pagination.rightButton(10), is("<li><a href=/memos/"+1L+"/"+(6)+">></a></li>"));
+		assertThat(pagination.rightButton(10), is("<li><a href=/api/memos/"+1L+"/"+(6)+">></a></li>"));
 		pagination = Pagination.of(9, 1L);
-		assertThat(pagination.rightButton(10), is("<li><a href=/memos/"+1L+"/"+(10)+">></a></li>"));
+		assertThat(pagination.rightButton(10), is("<li><a href=/api/memos/"+1L+"/"+(10)+">></a></li>"));
 	}
 
 	@Test
 	public void makePagination() {
 		pagination = Pagination.of(0, 1L);
-		String pagination2 = "<li><a href=/memos/"+1L+"/"+1+">"+1+"</a></li>"+"<li><a href=/memos/"+1L+"/"+2+">"+2+"</a></li>"+"<li><a href=/memos/"+1L+"/"+3+">"+3+"</a></li>"+"<li><a href=/memos/"+1L+"/"+4+">"+4+"</a></li>"+"<li><a href=/memos/"+1L+"/"+5+">"+5+"</a></li>";
+		String pagination2 = "<li><a href=/api/memos/"+1L+"/"+1+">"+1+"</a></li>"+"<li><a href=/api/memos/"+1L+"/"+2+">"+2+"</a></li>"+"<li><a href=/api/memos/"+1L+"/"+3+">"+3+"</a></li>"+"<li><a href=/api/memos/"+1L+"/"+4+">"+4+"</a></li>"+"<li><a href=/api/memos/"+1L+"/"+5+">"+5+"</a></li>";
 		assertThat(pagination.pagination(6), is(pagination2));
 	}
 
