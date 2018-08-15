5f1893c995e0064d65eb99009aec5d5e23ce794d
durin93 <lsc401@naver.com>
2018-08-15 15:04:36 +0900

    update spring profile

--- src/test/java/com/durin/web/AcceptanceTest.java
+++ src/test/java/com/durin/web/AcceptanceTest.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.durin.domain.User;
